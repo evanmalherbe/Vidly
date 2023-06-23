a2c83a2dee024b9d1cd5be07501a238f6c258d94
Evan Malherbe <evan@ix.co.za>
2023-06-23 15:05:14 +0200

    Added settings to format json responses in camelCase instead of PascalCase

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,21 +1,28 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Xml;
 
 namespace Vidly
 {
-    public static class WebApiConfig
-    {
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			var settings = config.Formatters.JsonFormatter.SerializerSettings;
+			settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+			settings.Formatting = Newtonsoft.Json.Formatting.Indented;
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-        }
-    }
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+					name: "DefaultApi",
+					routeTemplate: "api/{controller}/{id}",
+					defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
 }
