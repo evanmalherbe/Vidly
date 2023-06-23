3e0924e2007bb84aba9d44583ee4d7b7202ef953
Evan Malherbe <evan@ix.co.za>
2023-06-23 14:45:17 +0200

    Added Automapper and made necessary changes to customers controller api

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -5,18 +6,20 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Vidly.App_Start;
 
 namespace Vidly
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-    }
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			Mapper.Initialize(i => i.AddProfile<MappingProfile>());
+			GlobalConfiguration.Configure(WebApiConfig.Register);
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+		}
+	}
 }
