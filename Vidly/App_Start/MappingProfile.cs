9f8208d18450bcd620e15f667965a613e422fbad
Evan Malherbe <evan@ix.co.za>
2023-06-26 16:28:11 +0200

    Got movies api controller to work like customer controller for exercise

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,7 +12,12 @@ namespace Vidly.App_Start
 	{
 		public MappingProfile()
 		{
+			//Mapper.CreateMap<Customer, CustomerDto>()
+			//	.ForMember(c => c.Id, opt => opt.Ignore()).ReverseMap();
 			Mapper.CreateMap<Customer, CustomerDto>().ReverseMap();
+			Mapper.CreateMap<Movie, MovieDto>().ReverseMap();
+			//Mapper.CreateMap<Movie, MovieDto>()
+			//	.ForMember(m => m.Id, opt => opt.Ignore()).ReverseMap();
 		}
 	}
 }
