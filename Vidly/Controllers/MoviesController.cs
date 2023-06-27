c0f2df81d78f0476fb4c98b3eb7e7b457cb6b00d
Evan Malherbe <evan@ix.co.za>
2023-06-27 11:55:43 +0200

    Seeded users and gave movies an alternative page for users without admin rights.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -9,157 +9,162 @@ using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
-    public class MoviesController : Controller
-    {
-      private ApplicationDbContext _context;
+	public class MoviesController : Controller
+	{
+		private ApplicationDbContext _context;
 
-      public MoviesController()
-      {
-        _context = new ApplicationDbContext();
-      }
+		public MoviesController()
+		{
+			_context = new ApplicationDbContext();
+		}
 
-      protected override void Dispose(bool disposing)
-      {
-        _context.Dispose();
-      }
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+		}
 
-      public ActionResult New(int? id)
-      {
-        if (id == null)
-        {
-          var genres = _context.Genres.ToList();
-          var viewModel = new NewMovieViewModel
-          {
-            Genres = genres
-          };
+		public ActionResult New(int? id)
+		{
+			if (id == null)
+			{
+				var genres = _context.Genres.ToList();
+				var viewModel = new NewMovieViewModel
+				{
+					Genres = genres
+				};
 
-          return View(viewModel); 
-        }
-        else
-        {
-          var movieToUpdate = _context.Movies.SingleOrDefault(m => m.Id == id);
-          var genres = _context.Genres.ToList();
-          var viewModel = new NewMovieViewModel(movieToUpdate)
-          {
-            Genres = genres            
-          };
+				return View(viewModel);
+			}
+			else
+			{
+				var movieToUpdate = _context.Movies.SingleOrDefault(m => m.Id == id);
+				var genres = _context.Genres.ToList();
+				var viewModel = new NewMovieViewModel(movieToUpdate)
+				{
+					Genres = genres
+				};
 
-          return View(viewModel); 
-        }
-      }
+				return View(viewModel);
+			}
+		}
 
-      [HttpPost]
-      [ValidateAntiForgeryToken]
-      public ActionResult Save(Movie movie)
-      {
-         if (!ModelState.IsValid)
-          {
-            var viewModel = new NewMovieViewModel(movie) 
-            { 
-              Genres = _context.Genres
-            };
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Save(Movie movie)
+		{
+			if (!ModelState.IsValid)
+			{
+				var viewModel = new NewMovieViewModel(movie)
+				{
+					Genres = _context.Genres
+				};
 
-            return View("new", viewModel);
-          }
-        
-        if (movie.Id == 0)
-        {
-          movie.DateAdded = DateTime.Now;
-          _context.Movies.Add(movie);
-        }
-        else
-        {
-          var oldMovie = _context.Movies.Single(c => c.Id == movie.Id);
+				return View("new", viewModel);
+			}
 
-          oldMovie.Name = movie.Name;
-          oldMovie.ReleaseDate = movie.ReleaseDate;
-          oldMovie.DateAdded = movie.DateAdded;
-          oldMovie.NumberInStock = movie.NumberInStock;
-          oldMovie.GenreId = movie.GenreId;
-        }
-      
-        _context.SaveChanges();
+			if (movie.Id == 0)
+			{
+				movie.DateAdded = DateTime.Now;
+				_context.Movies.Add(movie);
+			}
+			else
+			{
+				var oldMovie = _context.Movies.Single(c => c.Id == movie.Id);
 
-        return RedirectToAction("Index", "Movies");
-      }
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            Movie movie = new Movie() {Name = "Shrek!" };
-            //ViewData["Movie"] = movie;
-            //ViewBag.Movie = movie;
+				oldMovie.Name = movie.Name;
+				oldMovie.ReleaseDate = movie.ReleaseDate;
+				oldMovie.DateAdded = movie.DateAdded;
+				oldMovie.NumberInStock = movie.NumberInStock;
+				oldMovie.GenreId = movie.GenreId;
+			}
 
-            var customers = new List<Customer>
-            {
-              new Customer {Name = "Customer 1"},
-              new Customer {Name = "Customer 2"}
-            };
+			_context.SaveChanges();
 
-            var viewModel = new RandomMovieViewModel
-            {
-              Movie = movie,
-              Customers = customers
-            };
+			return RedirectToAction("Index", "Movies");
+		}
+		// GET: Movies/Random
+		public ActionResult Random()
+		{
+			Movie movie = new Movie() { Name = "Shrek!" };
+			//ViewData["Movie"] = movie;
+			//ViewBag.Movie = movie;
 
-            return View(viewModel);
-            //return Content("Hello world!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
-        }
+			var customers = new List<Customer>
+						{
+							new Customer {Name = "Customer 1"},
+							new Customer {Name = "Customer 2"}
+						};
 
-        public ActionResult Edit(int id)
-        {
-          var movieToUpdate = _context.Movies.SingleOrDefault(m => m.Id == id);
-          if (movieToUpdate == null)
-          {
-            return RedirectToAction("Index", "Movies");
-          }
+			var viewModel = new RandomMovieViewModel
+			{
+				Movie = movie,
+				Customers = customers
+			};
 
-          var viewModel = new NewMovieViewModel(movieToUpdate)
-          {
-            Genres = _context.Genres.ToList(),
-          };
+			return View(viewModel);
+			//return Content("Hello world!");
+			//return HttpNotFound();
+			//return new EmptyResult();
+			//return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+		}
 
-          return View(viewModel);
-        }
+		public ActionResult Edit(int id)
+		{
+			var movieToUpdate = _context.Movies.SingleOrDefault(m => m.Id == id);
+			if (movieToUpdate == null)
+			{
+				return RedirectToAction("Index", "Movies");
+			}
 
-        public ActionResult Index()
-        {
-          var movies = _context.Movies.Include(g => g.Genre).ToList();
+			var viewModel = new NewMovieViewModel(movieToUpdate)
+			{
+				Genres = _context.Genres.ToList(),
+			};
 
-          var viewModel = new MovieViewModel
-          {
-            Movie = new Movie(),
-            Movies = movies
-          };
+			return View(viewModel);
+		}
 
-          return View(viewModel);
-        }
+		public ActionResult Index()
+		{	
+			var movies = _context.Movies.Include(g => g.Genre).ToList();
 
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
+				var viewModel = new MovieViewModel
+				{
+					Movie = new Movie(),
+					Movies = movies
+				};
 
-          return Content(year + "/" + month);
-        }
+			if (User.IsInRole("CanManageMovies"))
+			{
+					return View("Index", viewModel);
+			}
+		
+			return View("ReadOnlyList", viewModel);
+		}
 
-        // GET: /movies/details/1
-        public ActionResult Details(int id)
-        {
-          if (id == 0)
-          {
-             return HttpNotFound();
-          }
+		[Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+		public ActionResult ByReleaseDate(int year, int month)
+		{
 
-          Movie movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+			return Content(year + "/" + month);
+		}
 
-          if (movie == null)
-          {
-            return HttpNotFound();
-          }
+		// GET: /movies/details/1
+		public ActionResult Details(int id)
+		{
+			if (id == 0)
+			{
+				return HttpNotFound();
+			}
 
-          return View(movie);
-        }
-    }
+			Movie movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(c => c.Id == id);
+
+			if (movie == null)
+			{
+				return HttpNotFound();
+			}
+
+			return View(movie);
+		}
+	}
 }
---
+++ Vidly/Migrations/202306270926471_SeedUsers.cs
@@ -0,0 +1,27 @@
+﻿namespace Vidly.Migrations
+{
+	using System;
+    using System.Data.Entity.Migrations;
+	public partial class SeedUsers : DbMigration
+	{
+		public override void Up()
+		{
+			Sql(@"  INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'350e65ad-c7e3-412f-ae41-e62945f7cb4d', N'admin@vidly.com', 0, N'AC7115u2wRD7n3+IYuvgSuKC3vZ9v0PCut2Q6t+UIRur+kjOrLvHgP+sQJhihCcZNg==', N'54f36dc2-4b5a-452f-945f-6f7d0a574fb4', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+
+			INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'c0993942-31e8-4046-b11c-2bc4f6f92bfd', N'guest@vidly.com', 0, N'AAGxJs2lQVY9DExBY5hZ00jc56GT9peojMnic3TyaTFBB6taiPK6LuhyGtPV+o2MDg==', N'92ed50a0-3379-4da9-9cbc-2876e73422af', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+
+			INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'cffba3f8-b03c-42ee-9f8c-9671dafcef88', N'CanManageMovies')
+
+			INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'350e65ad-c7e3-412f-ae41-e62945f7cb4d', N'cffba3f8-b03c-42ee-9f8c-9671dafcef88')
+
+			");
+        
+
+
+
+				}
+		public override void Down()
+		{
+		}
+    }
+}
