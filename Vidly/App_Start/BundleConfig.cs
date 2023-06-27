9adb95b4f8ddf95f08e7fcdcadf02ac75bc32135
Evan Malherbe <evan@ix.co.za>
2023-06-27 11:01:43 +0200

    Added Datatables to project and use it on customers page.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace Vidly
     public static void RegisterBundles(BundleCollection bundles)
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
+                  "~/Scripts/jquery-{version}.js",
+                  "~/scripts/datatables/jquery.datatables.js",
+                  "~/scripts/datatables/datatables.bootstrap.js"));
 
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                   "~/Scripts/jquery.validate*"));
@@ -20,11 +22,14 @@ namespace Vidly
                   "~/Scripts/modernizr-*"));
 
       bundles.Add(new Bundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js",
+                "~/scripts/bootbox.js"));
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap-lumen.css",
-                "~/Content/site.css"));
+                "~/content/datatables/css/datatables.bootstrap.css",
+                "~/Content/site.css"
+                ));
     }
   }
 }
