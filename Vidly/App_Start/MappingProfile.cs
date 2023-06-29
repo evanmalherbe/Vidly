861696b77b1559bbda682e600f54650d51c7ed42
Evan Malherbe <evan@ix.co.za>
2023-06-29 14:22:00 +0200

    Fixed delete button on movies and customers tables

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -12,12 +12,9 @@ namespace Vidly.App_Start
 	{
 		public MappingProfile()
 		{
-			//Mapper.CreateMap<Customer, CustomerDto>()
-			//	.ForMember(c => c.Id, opt => opt.Ignore()).ReverseMap();
 			Mapper.CreateMap<Customer, CustomerDto>().ReverseMap();
 			Mapper.CreateMap<Movie, MovieDto>().ReverseMap();
-			//Mapper.CreateMap<Movie, MovieDto>()
-			//	.ForMember(m => m.Id, opt => opt.Ignore()).ReverseMap();
+			Mapper.CreateMap<MembershipType, MembershipTypeDto>().ReverseMap();
 		}
 	}
 }
--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -19,6 +19,7 @@ namespace Vidly.Dtos
 		public bool IsSubscribedToNewsletter { get; set; }
 
 		public byte MembershipTypeId { get; set; }
+		public MembershipTypeDto MembershipType { get; set; }
 
 		//[Min18YearsIfAMember]
 		public DateTime? Birthdate { get; set; }
