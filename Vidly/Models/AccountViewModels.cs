c69568169ef9894e1b0aeaeb7020ba6ef09b57f7
Evan Malherbe <evan@ix.co.za>
2023-06-29 20:42:18 +0200

    Moved some models and classes to external files as suggested by Mosh for the exercise at the end of Social Logins section 8 (lesson 98)

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -3,18 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-
-        [Required]
-        [Display(Name = "Driving License")]
-        public string DrivingLicense { get; set; }
-    }
-
-    public class ExternalLoginListViewModel
+     public class ExternalLoginListViewModel
     {
         public string ReturnUrl { get; set; }
     }
---
+++ Vidly/Models/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Vidly.Models
+{
+   public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+    }
+}
